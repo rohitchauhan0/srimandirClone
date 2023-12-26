8c8bb08e0ff6984d4f81222408e8bca589043ed4
rohitchauhan0 <beatfeelrc121@gmail.com>
2023-12-26 21:05:21 +0530

    third commit

--- src/component/common/Navbar.js
+++ src/component/common/Navbar.js
@@ -19,8 +19,8 @@ const Navbar = () => {
                         NavLinks.map((data)=>{
                             return <Link className=' hover:text-orange-700 transition-all duration-200' to={data.path}>
                                 {
-                                    data.title === "Library" ? (<div className=' flex items-center gap-2 relative'><p>Library</p>
-                                    <FaAngleDown onClick={()=> setshowLibrary(!showLibrary)} />
+                                    data.title === "Library" ? (<div className=' flex items-center gap-2 relative' onClick={()=> setshowLibrary(!showLibrary)}><p>Library</p>
+                                    <FaAngleDown  />
                                     <div className={`${showLibrary ? "hidden":"visible"} absolute bg-white rounded-xl p-3 translate-y-[53%] -translate-x-[30%] border-[1px] border-gray-400 min-w-[500px] h-[500px] z-[2000] transition-all duration-200 flex flex-col gap-5`}>
                                     {
                                         libraryLinks.map((data)=>{
